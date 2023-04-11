c7f2b586084cfd4790e7f617c7e4a372e6a4e8b8
Mayank Kumar Sharma <mayanksharma6979@gmail.com>
2023-04-11 19:01:22 +0530

    Node added

--- LearningGraph.java
+++ LearningGraph.java
@@ -1,6 +1,8 @@
+import java.util.*;
+
 class LearningGraph{
     // creating the class edge.
-    class Edge{
+   static class Edge{
         int src;
         int dest;
 
@@ -12,7 +14,36 @@ class LearningGraph{
 
     }
 
-    public static void main(String [] args){
 
+    // creating the method to create graph
+
+    public static void createGraph(ArrayList<Edge> graph []){
+    for(int i=0; i<graph.length; i++){
+        graph[i] = new ArrayList<Edge>();
+    }
+
+    // adding node into the graph array.
+    // add function is already defined in the ArrayList class.
+
+    graph[0].add(new Edge(0,2));
+
+    graph[1].add(new Edge(1,2));
+    graph[1].add(new Edge(1, 3));
+
+    graph[2].add(new Edge(2,0));
+    graph[2].add(new Edge(2,1));
+    graph[2].add(new Edge(2,3));
+
+    graph[3].add(new Edge(3,1));
+    graph[3].add(new Edge(3,2));
+
+}
+
+    public static void main(String [] args){
+        int V = 4;
+
+        // creating the graph array of Arraylist type Edge and size V=4;
+
+        ArrayList<Edge> graph [] = new ArrayList [V];
     }
 }
