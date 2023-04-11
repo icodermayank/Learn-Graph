d5d9db764321a2030c61291d729a4ad1dbdf9d11
Mayank Kumar Sharma <mayanksharma6979@gmail.com>
2023-04-11 18:42:19 +0530

    edge class added

--- LearningGraph.java
+++ LearningGraph.java
@@ -1,5 +1,17 @@
 class LearningGraph{
-    
+    // creating the class edge.
+    class Edge{
+        int src;
+        int dest;
+
+        // creating constructor to initilize the value
+        Edge(int source, int dest){
+            this.src = source; 
+            this.dest = dest;
+        }
+
+    }
+
     public static void main(String [] args){
 
     }
