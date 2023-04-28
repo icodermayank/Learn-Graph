a3e797678afc5834885cf13fb57a4fad337a92e6
Mayank Kumar Sharma <mayanksharma6979@gmail.com>
2023-04-28 17:17:11 +0530

    Dijkstra Algorithm Sortest Path

--- DijkstraAlgorithm.java
+++ DijkstraAlgorithm.java
@@ -20,12 +20,88 @@ static class Edge{
         }
 
         // adding node to the graph
+        graph[0].add(new Edge(0,1,2));
+        graph[0].add(new Edge(0,2,4));
+        graph[1].add(new Edge(1,2,1));
+        graph[1].add(new Edge(1,3,7));
+        graph[2].add(new Edge(2,4,3));
+        graph[3].add(new Edge(3,5,1));
+        graph[4].add(new Edge(4,3,2));
+        graph[4].add(new Edge(4,5,5));
+
+
+    }
+    // pair class
+    public static class Pair implements Comparable<Pair>{
+        int node;
+        int dist;
+        public Pair(int n, int d){
+            this.node = n;
+            this.dist = d;
+        }
+
+        @Override
+        public int compareTo(Pair p2){
+            return this.dist - p2.dist; // ascending
+        }
+    }
+
+
+    // Dijkstra Alogrithm
+
+    public static void dijkstra(ArrayList<Edge> graph [], int src, int V){
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+    
+        int  dist [] = new int [V];
+    
+        for(int i=0; i<V; i++){
+            if(i!=src){
+                dist[i] = Integer.MAX_VALUE;
+            }
+        }
+    
+        boolean vis[] = new boolean[V];
+    
+        pq.add(new Pair(0, 0));
+    
+        while(!pq.isEmpty()){
+            Pair cur = pq.remove(); // priority queue returns shortest distance.
+            if(!vis[cur.node]){
+                vis[cur.node] = true; // mark as visited.
+            
+                for(int i =0; i<graph[cur.node].size(); i++){
+                    Edge e = graph[cur.node].get(i);
+    
+                    int u = e.src;
+                    int v = e.dest;
+    
+                    // relaxation
+                    if(dist[u]+e.wt< dist[v] ){
+                        dist[v] = dist[u]+e.wt;
+    
+                        pq.add(new Pair(v, dist[v]));
+                    }
+    
+                }
+            
+            }
+    
+            
+        }
+    
+        for(int i=0; i<V; i++){
+            System.out.print(dist[i]+" ");
+        }
+        System.out.println();
+    
     }
     public static void main(String [] args){
 
-        int v =  8;
-        ArrayList<Edge> graph [] = new ArrayList[v];
+        int  V=  6;
+        ArrayList<Edge> graph [] = new ArrayList[V];
 
         createGraph(graph);
+
+        dijkstra(graph, 0, V);
     }
 }
